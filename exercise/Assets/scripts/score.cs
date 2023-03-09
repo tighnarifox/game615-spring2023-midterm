90f9921c4f1ca8a5c433fd5839fb32c43077b844
tighnarifox <123525841+tighnarifox@users.noreply.github.com>
2023-03-08 19:11:04 -0500

    exercise

---
+++ exercise/Assets/scripts/score.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class score : MonoBehaviour
+{
+    [SerializeField] GameObject beer;
+    [SerializeField] GameObject salad;
+    [SerializeField] GameObject spawnpoint;
+    public TMP_Text scoreText;
+    
+    // Start is called before the first frame update
+    public void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "Start by finding your way home. Press space, WASD and go to your friends for salad and beer to get energy to find your way back.";
+    }
+
+void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+    
+            scoreText.text = "you dropped your lyre king";
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+    
+            scoreText.text = "watch where you're heading";
+        }
+          if (Input.GetKeyDown("a"))
+        {
+    
+            scoreText.text = "go find xinyan for beer";
+        }
+
+          if (Input.GetKeyDown("s"))
+        {
+    
+            scoreText.text = "go find albedo for salad";
+        }
+
+          if (Input.GetKeyDown("d"))
+        {
+    
+            scoreText.text = "you aren't home yet? it's getting late";
+        }
+        
+    }
+      public void OnTriggerEnter(Collider other)
+     {
+        if (other.CompareTag("xinyan"))
+        {
+            Destroy(other.gameObject);
+            Instantiate(beer,spawnpoint.transform.position, Quaternion.identity);
+
+
+        }
+    
+       if (other.CompareTag("albedo"))
+       {    
+            Destroy(other.gameObject);
+            Instantiate(salad,spawnpoint.transform.position, Quaternion.identity);
+            
+       }
+
+       if (other.CompareTag("house"))
+         {  
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+         
+         }
+        
+     }
+}
+
+ 
